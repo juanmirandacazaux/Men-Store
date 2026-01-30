30a12d46f6ee45af19a0b589d3d392ef54f3ce46
juanmirandacazaux <juanmirandacazaux@gmail.com>
2026-01-30 17:24:44 -0300

    Products updates

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 const products = [
   { id: 1, name: "Urban Jacket", price: 120, category: "jackets", img: "../assets/JacketBlue.jpg" },
-  { id: 2, name: "Classic Hoodie", price: 90, category: "hoodies", img: "../assets/p2.jpg" },
-  { id: 3, name: "Sport Sneakers", price: 140, category: "shoes", img: "../assets/p3.jpg" },
+  { id: 2, name: "Classic Hoodie", price: 90, category: "hoodies", img: "../assets/Classic-Hoodie.jpg" },
+  { id: 3, name: "Sport Sneakers", price: 140, category: "shoes", img: "../assets/Sport-Sneakers.jpg" },
   { id: 4, name: "Basic T-Shirt", price: 40, category: "tshirts", img: "../assets/p4.jpg" },
   { id: 5, name: "Denim Shirt", price: 75, category: "shirts", img: "../assets/p5.jpg" },
   { id: 6, name: "Premium Jacket", price: 180, category: "jackets", img: "../assets/p6.jpg" },
-  { id: 7, name: "Premium Black Jacket", price: 200, category: "jackets", img: "../assets/Black-Jacket.jpg" }
+  { id: 7, name: "Premium Black Jacket", price: 200, category: "jackets", img: "../assets/Black-Jacket.jpg" },
+  { id: 8, name: "Sport Hoodie", price: 70, category: "hoodies", img: "../assets/skyblue-hoodie.jpg" },
+  { id: 9, name: "White Hoodie", price: 56, category: "hoodies", img: "../assets/white-hoodie.jpg" }
 ];
